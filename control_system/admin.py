dc4e32d7d85646c3cde84012f07a6b6fe9ada9bf
asad github <asadsayyad233@gmail.com>
2024-09-16 12:05:14 +0530

    added

--- control_system/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Student, Teacher, Principal
-
-admin.site.register(Student)
-admin.site.register(Teacher)
-admin.site.register(Principal)
