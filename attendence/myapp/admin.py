62ffe67f21cda65eda0e194dedd04b7e523448e1
kaditya67 <322adityakumar0004@dbit.in>
2024-09-16 12:22:47 +0530

    Models updated

--- attendence/myapp/admin.py
+++ attendence/myapp/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+import myapp.models as models 
+
+
+admin.site.register(models.Department)
+admin.site.register(models.Year)
+admin.site.register(models.Program)
+admin.site.register(models.Teacher)
+admin.site.register(models.Student)
+admin.site.register(models.OddSem)
+admin.site.register(models.EvenSem)
+admin.site.register(models.HonorsMinors)
+admin.site.register(models.LabsBatches)
+admin.site.register(models.Lecture)
